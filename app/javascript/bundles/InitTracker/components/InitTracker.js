581e714e8980f7d1e0621ae1f831895286d636b2
Thomas Wilson <twilson.spqr@gmail.com>
2017-12-04 22:37:35 -0500

    copying my own work

--- app/javascript/bundles/InitTracker/components/InitTracker.js
+++ app/javascript/bundles/InitTracker/components/InitTracker.js
@@ -1,45 +1,52 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import ErrorBlock from './ErrorBlock';
+import TableLine from './TableLine';
+import TableBlock from './TableBlock';
+import RaceSelect from './RaceSelect';
 
-export default class InitTracker extends React.Component {
-  static propTypes = {
-    name: PropTypes.string.isRequired, // this is passed from the Rails view
-  };
-
-  /**
-   * @param props - Comes from your rails view.
-   */
+export default class PBCalc extends React.Component {
   constructor(props) {
     super(props);
-
-    // How to set initial state in ES6 class syntax
-    // https://facebook.github.io/react/docs/reusable-components.html#es6-classes
-    this.state = { name: this.props.name };
+      this.state = {
+        errors: [],
+        stats: {
+          strength: {text: "Strength", name: 10, value: 0},
+          constitution: {text: "Constitution", name: 10, value: 0},
+          dexterity: {text: "Dexterity", name: 10, value: 0},
+          intelligence: {text: "Intelligence", name: 10, value: 0},
+          wisdom: {text: "Wisdom", name: 10, value: 0},
+          charisma: {text: "Charisma", name: 10, value: 0}
+        },
+        selectedRace: this.props.raceStats[0],
+        selectedStat: '',
+        pointTotal: 0
+      }
   }
-
-  updateName = (name) => {
-    this.setState({ name });
-  };
 
   render() {
     return (
-      <div>
-        <h3>
-          Hello, {this.state.name}!
-        </h3>
-        <hr />
-        <form >
-          <label htmlFor="name">
-            Say hello to:
-          </label>
-          <input
-            id="name"
-            type="text"
-            value={this.state.name}
-            onChange={(e) => this.updateName(e.target.value)}
-          />
-        </form>
-      </div>
+      <form>
+      <h3>Calculate Stats</h3>
+        <ErrorBlock
+          errors={this.state.errors}
+        />
+        <RaceSelect
+          selectedRace={this.state.selectedRace}
+          raceSelect={this.raceSelect}
+          handleRaceChange={this.handleRaceChange}
+          raceStats={this.props.raceStats}
+          selectedStat={this.state.selectedStat}
+        />
+        <br/>
+        <TableBlock
+          stats={this.state.stats}
+          handleStatChange={this.handleStatChange}
+          selectedRace={this.state.selectedRace}
+          statValues={this.props.statValues}
+          pointTotal={this.state.pointTotal}
+        />
+      </form>
     );
   }
 }
