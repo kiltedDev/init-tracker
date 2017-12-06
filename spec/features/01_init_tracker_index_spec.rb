0ef36316f390159d9dd8afecdb1c02ed6d0e0106
Thomas Wilson <twilson.spqr@gmail.com>
2017-12-06 16:42:30 -0500

    copying my own work

--- spec/features/01_init_tracker_index_spec.rb
+++ spec/features/01_init_tracker_index_spec.rb
@@ -9,12 +9,12 @@ feature "see init tracker form on entry" do
 
     expect(page).to have_content("Salutations!")
     expect(page).to have_content("How many players are there?")
-    expect(page).to have_field("#player_count")
+    expect(page).to have_field("#pcCount")
   end
 
   scenario "user asked for number of NPCs" do
       expect(page).to have_content("How many NPCs are there?")
-      expect(page).to have_field("#npc_count")
+      expect(page).to have_field("#npcCount")
   end
 
   scenario "page has creation button" do
@@ -22,8 +22,8 @@ feature "see init tracker form on entry" do
   end
 
   scenario "user creates table" do
-    fill_in 'player_count', with: 5
-    fill_in 'npc_count', with: 2
+    fill_in 'pcCount', with: 5
+    fill_in 'npcCount', with: 2
 
     click_button 'Create Tracker'
 
@@ -35,8 +35,8 @@ end
 
 feature "table created from relevant information" do
   before(:each) do
-    fill_in 'player_count', with: 5
-    fill_in 'npc_count', with: 2
+    fill_in 'pcCount', with: 5
+    fill_in 'npcCount', with: 2
 
     click_button 'Create Tracker'
   end
