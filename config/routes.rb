0b840d89c822d043b0ebf4d05a22e435fb49ca03
Thomas Wilson <twilson.spqr@gmail.com>
2017-12-14 23:07:09 -0500

    cleaning tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'hello_world', to: 'hello_world#index'
   get 'init_tracker', to: 'init_tracker#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
