ba022e762e9a3423c1bcabf6d061ff5a0feb73a8
Thomas Wilson <twilson.spqr@gmail.com>
2017-12-14 23:14:14 -0500

    routes cleaned up

--- app/javascript/packs/init-tracker-bundle.js
+++ app/javascript/packs/init-tracker-bundle.js
@@ -1,6 +1,6 @@
 import ReactOnRails from 'react-on-rails';
 
-import HelloWorld from '../bundles/InitTracker/components/InitTracker';
+import InitTracker from '../bundles/InitTracker/components/InitTracker';
 
 // This is how react_on_rails can see the InitTracker in the browser.
 ReactOnRails.register({
